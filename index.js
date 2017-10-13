6ace69e512877fdc099bcc8ea98ab4d89c954d87
Darklit <0lies18pies@gmail.com>
2017-10-13 17:08:07 -0400

    Create word scrambling method and tweet random things.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const twit = require("twit");
 const config = require("./config.js");
+const words = require("./words.js");
 var recentTweet = {
   text: "nothing"
 };
+var sendTweets = [];
 
 const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
@@ -22,8 +24,13 @@ function getUsers(searchTerm,fn,f1){
     q: searchTerm,
     page: Math.floor(Math.random()*10)
   }, (err,data,body) => {
-    if(err) return;
-    screen_name = (data[Math.floor(Math.random()*data.length)].screen_name);
+    if(err) throw err;
+    if(searchTerm == "lmao_ian"){
+      screen_name = "Lmao_Ian";
+    }else{
+      screen_name = (data[Math.floor(Math.random()*data.length)].screen_name);
+    }
+    console.log('please');
     fn(screen_name,f1);
   });
 }
@@ -33,7 +40,7 @@ function followUser(screenname){
   Twitter.post('friendships/create',{
     screen_name: screenname
   }, function(err,data,body){
-    if(err) return;
+    if(err) throw err;
     console.log(`Followed ${data.screen_name}`);
   });
 }
@@ -42,20 +49,53 @@ function nothing(body){
   console.log(body);
 }
 
+function sendTweet(tweet){
+  console.log("sending tweet...");
+  Twitter.post('statuses/update',{
+    status: tweet
+  },(err,data,body) => {
+    if(err) return;
+    console.log(data.text);
+  });
+}
+
+function copyThis(){
+  const users = [
+    "lmao_ian",
+    "Nebuchadneezar",
+    "dril",
+    "imkellam",
+    "BradWray",
+    "carder_bot",
+    "PapaJohns"
+  ];
+  getUsers(users[Math.floor(Math.random()*users.length)],getTweet,setStuff);
+  console.log("ran");
+}
+
+
+function setStuff(data){
+  console.log("setStuff");
+  sendTweet(words.scramble(data));
+}
+
 function getTweet(screenname,fn){
+  console.log("started");
   Twitter.get('statuses/user_timeline',{
     screen_name: screenname
   },(err,data,body) => {
+    if(err) return;
     //console.log(data[0].text);
     if(screenname.toLowerCase() == 'lmao_ian'){
-      if(recentTweet.text != data[0].text){
-        //console.log('here');
-        recentTweet = data[0];
-        carderBot.emit('newTweet');
-        //console.log(recentTweet);
-      }
+      //console.log('here');
+      recentTweet = data[0];
+      carderBot.emit('newTweet');
+      //console.log(recentTweet);
+      //console.log("here bitch");
+      if(fn != null) fn(data);
     }else{
-      fn(data);
+      //console.log(data[0]);
+      if(fn != null) fn(data);
     }
   });
 }
@@ -76,9 +116,14 @@ carderBot.on('newTweet',function(){
   retweet(recentTweet);
 });
 setInterval(function(){
+  copyThis();
+},5000);
+/*
+setInterval(function(){
   getTweet("lmao_ian",null);
 },1000);
 
 setInterval(function(){
   getUsers(letters[Math.floor(Math.random()*letters.length)],followUser,null);
 },10000);
+*/
---
+++ words.js
@@ -0,0 +1,63 @@
+module.exports = {
+  scramble: function(data){
+    console.log(data);
+    if(data[0] == undefined){
+      console.log("broke");
+      return '';
+    }else{
+      var tweet1 = data[Math.floor(Math.random()*data.length)].text;
+      var tweet2 = data[Math.floor(Math.random()*data.length)].text;
+      var tweet3 = data[Math.floor(Math.random()*data.length)].text;
+      var tries = 0;
+      while(tweet1 == tweet2 || tweet1==tweet3 || tweet2 == tweet3){
+        console.log("stuck");
+        tweet1 = data[Math.floor(Math.random()*data.length)].text;
+        tweet2 = data[Math.floor(Math.random()*data.length)].text;
+        tweet3 = data[Math.floor(Math.random()*data.length)].text;
+        tries++;
+        if(tries>10000){
+          return '';
+        }
+      }
+      while(tweet1.includes("RT")){
+        tweet1 = data[Math.floor(Math.random()*data.length)].text;
+      }
+      while(tweet2.includes("RT")){
+        tweet2 = data[Math.floor(Math.random()*data.length)].text;
+      }
+      while(tweet3.includes("RT")){
+        tweet3 = data[Math.floor(Math.random()*data.length)].text;
+      }
+      var tweet1Array = tweet1.split(" ");
+      var tweet2Array = tweet2.split(" ");
+      var tweet3Array = tweet3.split(" ");
+
+      var random1 = Math.floor((Math.random()*tweet1Array.length));
+      var random2 = (Math.floor((Math.random()*tweet2Array.length)))+random1;
+      var random3 = (Math.floor((Math.random()*tweet3Array.length)))+random2;
+      console.log(tweet1);
+      console.log(tweet2);
+      console.log(tweet3);
+      var newTweet = "";
+      for(var i = 0; i <= random1; i++){
+      //  console.log("1+");
+      //  console.log(i);
+      if(tweet1Array[i] != undefined)  newTweet+=tweet1Array[i] + " ";
+      }
+      for(var i = random1; i <= random2; i++){
+      //  console.log("2+");
+      //console.log(i);
+      if(tweet2Array[i] != undefined)  newTweet+=tweet2Array[i] + " ";
+      }
+      for(var i = random2; i <= random3; i++){
+      //  console.log("3+");
+    //    console.log(i);
+      if(tweet3Array[i] != undefined)  newTweet+=tweet3Array[i] + " ";
+      }
+      while(newTweet.includes('@')){
+        newTweet = newTweet.replace('@','');
+      }
+      return newTweet.replace('@','');
+    }
+  }
+};
