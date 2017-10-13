82ab5a0570cbd35fb27da76c274a97b3eea5b8b7
Darklit <0lies18pies@gmail.com>
2017-10-13 13:27:47 -0400

    Revamp twitter bot and use twit npm package

--- index.js
+++ index.js
@@ -1,67 +1,84 @@
-const request = require("request");
+const twit = require("twit");
 const config = require("./config.js");
-var qs = require("querystring"), oauth = {
-  callback: "https://api.twitter.com",
+var recentTweet = {
+  text: "nothing"
+};
+
+const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+var events = require('events');
+var carderBot = new events.EventEmitter();
+
+var Twitter = new twit({
   consumer_key: config.consumer_key,
-  consumer_key_secret: config.consumer_key_secret
-}, url = 'https://api.twitter.com/oauth/request_token';
+  consumer_secret: config.consumer_key_secret,
+  access_token: config.access_token,
+  access_token_secret: config.access_token_secret
+});
 
-function authorize(){
-  request.post({
-    url: url,
-    oauth: oauth
-  }, function(err,res,body){
-    var req_data = qs.parse(body);
-    var uri = 'https://api.twitter.com/oauth/authenticate' + '?' + qs.stringify({
-      oauth_token: req_data.oauth_token
-    });
-    var auth_data = qs.parse(body),
-    oauth = {
-      consumer_key: config.consumer_key,
-      consumer_key_secret: config.consumer_key_secret,
-      token: auth_data.oauth_token,
-      token_secret: auth_data.oauth_token_secret,
-      verifier: auth_data.oauth_verifier
-    },
-    url = 'https://api.twitter.com/oauth/access_token';
-    request.post({
-      url:url,
-      oauth: oauth
-    }, (r,b,bod) =>{
-      console.log(b);
-      var perm_data = qs.parse(bod),
-      oauth = {
-        consumer_key: config.consumer_key,
-        consumer_key_secret: config.consumer_key_secret,
-        token: perm_data.oauth_token,
-        token_secret: perm_data.oauth_token
-      },
-      url = `https://api.twitter.com/1.1/users/search.json`,
-      qs = {
-        q: "test"
-      };
-      request.get({
-        url:url,
-        qs:qs,
-        oauth: oauth,
-        json: true
-      },function(e,re,b2){
-        if(e) throw e;
-        console.log(b2);
-      })
-    })
+function getUsers(searchTerm,fn,f1){
+  var screen_name = '';
+  Twitter.get('users/search',{
+    q: searchTerm,
+    page: Math.floor(Math.random()*10)
+  }, (err,data,body) => {
+    if(err) return;
+    screen_name = (data[Math.floor(Math.random()*data.length)].screen_name);
+    fn(screen_name,f1);
   });
 }
 
-function getUsers(searchTerm){
-  request.get({
-    url: `https://api.twitter.com/1.1/users/search.json?q=${searchTerm}&page=1&count=3`,
-    allowAllRedirects: true
-  }, function(err,res,body){
-    if (err) throw err;
-    console.log("Res: " + res);
-    console.log("Body: " + body);
+function followUser(screenname){
+  //console.log(screenname + " name");
+  Twitter.post('friendships/create',{
+    screen_name: screenname
+  }, function(err,data,body){
+    if(err) return;
+    console.log(`Followed ${data.screen_name}`);
   });
 }
 
-authorize();
+function nothing(body){
+  console.log(body);
+}
+
+function getTweet(screenname,fn){
+  Twitter.get('statuses/user_timeline',{
+    screen_name: screenname
+  },(err,data,body) => {
+    //console.log(data[0].text);
+    if(screenname.toLowerCase() == 'lmao_ian'){
+      if(recentTweet.text != data[0].text){
+        //console.log('here');
+        recentTweet = data[0];
+        carderBot.emit('newTweet');
+        //console.log(recentTweet);
+      }
+    }else{
+      fn(data);
+    }
+  });
+}
+
+function retweet(tweet){
+  console.log('ehy');
+  Twitter.post('statuses/retweet/:id',{
+    id: tweet.id_str
+  },(err,data,body) => {
+    if(err) return;
+    console.log("retweeted");
+  });
+}
+
+//getUsers("lmao_ian",getTweet,null);
+
+carderBot.on('newTweet',function(){
+  retweet(recentTweet);
+});
+setInterval(function(){
+  getTweet("lmao_ian",null);
+},1000);
+
+setInterval(function(){
+  getUsers(letters[Math.floor(Math.random()*letters.length)],followUser,null);
+},10000);
